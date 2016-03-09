78cf66decaa1c9aeb65976aed3a5f434ab676791
Tomáš Šimandl <tomas.simandl@gmail.com>
2016-03-09 11:34:31 +0100

    add markdown convertor add to gitignore .idea

--- server.py
+++ server.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python2
 
 import web
+import markdown
 
 urls = (
-	'/', 'Editor'
+    '/', 'Editor',
+    '/markdown', 'Markdown'
 )
 
 app = web.application(urls, globals())
 templates = web.template.render('templates')
 
+
 class Editor:
-	def GET(self):
-		return templates.editor()
+    def GET(self):
+        return templates.editor()
+
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
+
+class Markdown:
+    def POST(self):
+        data = web.input()
+        md = markdown.Markdown()
+        return md.convert(data.text)
