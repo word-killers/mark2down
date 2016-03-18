fa20628b138fe0b2323f5d9a014c91649c432d60
Tomáš Šimandl <tomas.simandl@gmail.com>
2016-03-19 00:21:23 +0100

    separate scripts from index.html

---
+++ static/script/scripts.js
@@ -0,0 +1,26 @@
+function putChar(char) {
+            var editor = document.getElementById("editor");
+            var CaretPos = 0;
+            if (document.selection) {
+                editor.focus();
+                var Sel = document.selection.createRange();
+                Sel.moveStart('character', -editor.value.length);
+                CaretPos = Sel.text.length;
+            }
+            else if (editor.selectionStart || editor.selectionStart == '0')
+                CaretPos = editor.selectionStart;
+
+            editor.value = editor.value.substring(0, CaretPos) + char + editor.value.substring(CaretPos);
+
+            if (editor.setSelectionRange) {
+                editor.focus();
+                editor.setSelectionRange(CaretPos, CaretPos);
+            }
+            else if (editor.createTextRange()) {
+                var range = editor.createTextRange();
+                range.collapse(true);
+                range.moveEnd('character', CaretPos);
+                range.moveStart('character', CaretPos);
+                range.select();
+            }
+        }
