ddf7a503f6142c035aaa52c1d81b808e402dfe1d
Tomáš Šimandl <tomas.simandl@gmail.com>
2016-04-02 02:28:07 +0200

    no message

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,5 +9,5 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-qunit');
 
-    grunt.registerTask('test', 'lint qunit');
+    grunt.registerTask('test', ['lint', 'qunit']);
 };
