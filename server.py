ed22790839c392d1089158b00fafb97761a5970b
Tomáš Šimandl <tomas.simandl@gmail.com>
2016-04-07 18:22:09 +0200

    table of content functional href

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import sys
 import web, urllib, requests
 import markdown
 import extension
+from markdown.extensions.toc import TocExtension
 
 # read client_id and client_secret from CLI, otherwise set to 0 (causes credentials err.)
 if len(sys.argv) == 4:
@@ -95,13 +96,15 @@ class Markdown:
                                [ext,
                                 'markdown.extensions.tables',  # tables
                                 'markdown.extensions.sane_lists',  # using lists like in normal mardkown
-                                'markdown.extensions.toc',
+                                TocExtension(slugify=self.code),
                                 # 'markdown_include.include' #option to include other files
                                 ])
 
         data = '<?xml version="1.0" encoding="utf-8" ?><reply><preview>' + md.convert(data['data']) + '</preview><toc>' + md.toc + '</toc><comments>'+ ext.comment_list +'</comments></reply>'
         return data
 
+    def code(self, value, separator):
+        return 'header'+separator+value
 
 class Test:
     def GET(self):
