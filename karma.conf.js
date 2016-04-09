f134d86b9c4d4200344ed158ef235da0f6324c31
Tomáš Šimandl <tomas.simandl@gmail.com>
2016-04-09 19:52:31 +0200

    oprava chyb pro nastaveni codecov a travis

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,7 @@
 module.exports = function (config) {
     config.set({
         basePath: '',
+        autoWatch: true,
         frameworks: ['qunit'],
         files: [
             'static/script*.js',
