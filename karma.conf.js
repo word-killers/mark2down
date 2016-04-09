addd624242bd5fa4341ddfe5bd36e247dc975e79
Tomáš Šimandl <tomas.simandl@gmail.com>
2016-04-09 20:00:35 +0200

    oprava chyb pro nastaveni codecov a travis

--- karma.conf.js
+++ karma.conf.js
@@ -7,26 +7,10 @@ module.exports = function (config) {
             'static/script*.js',
             'static/test.js'
         ],
-        plugins: [
-            'karma-coverage',
-            'karma-phantomjs-launcher'
-        ],
+
         browsers: ['PhantomJS'],
-        singleRun: true,
         reporters: ['progress', 'coverage'],
         preprocessors: {'*.js': ['coverage']},
-
-        coverageReporter: {
-            dir: 'coverage/',
-            reporters: [
-                {type: 'html', subdir: 'html'},
-                {type: 'lcovonly', subdir: 'lcov'},
-                {type: 'cobertura', subdir: 'cobertura'}
-            ]
-        }
-
-
+        singleRun: true
     });
-
-
 };
