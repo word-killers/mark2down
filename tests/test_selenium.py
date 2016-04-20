becf8309c9f2c43699fbdf541210fcdb1ccf269d
Tomáš Šimandl <tomas.simandl@gmail.com>
2016-04-20 17:57:35 +0200

    move test.py to folder tests and rename to tests_markdown.py, add first test for selenium

---
+++ tests/test_selenium.py
@@ -0,0 +1,26 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class PythonOrgSearch(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.get("localhost:8080")
+
+    def test_graph(self):
+        driver = self.driver
+        driver.find_element_by_id('btnGraph').click()
+        self.assertEqual("```graph\n\n```", driver.find_element_by_id("editor").get_attribute('value'))
+
+    def test_table(self):
+        driver = self.driver
+        driver.find_element_by_id('tableButton').click()
+        self.assertTrue(driver.find_element_by_id('dialog').is_displayed())
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
