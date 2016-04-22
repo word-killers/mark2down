b498b2e55454929b080856132dcda0f16f1691f6
Jiřina Chocholková <chocholkovaj@seznam.cz>
2016-04-22 11:20:57 +0200

    test btnH6

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -43,6 +43,11 @@ class PythonOrgSearch(unittest.TestCase):
         driver.find_element_by_id('btnH5').click()
         self.assertTrue("##### ", driver.find_element_by_id("editor").get_attribute('value'))
 
+    def test_H6_btn(self):
+        driver = self.driver
+        driver.find_element_by_id('btnH6').click()
+        self.assertTrue("###### ", driver.find_element_by_id("editor").get_attribute('value'))
+
     def tearDown(self):
         self.driver.close()
 
