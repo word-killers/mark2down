7a2fb46a163eda6c0700258bcf26cb914c7d3faa
Jiřina Chocholková <chocholkovaj@seznam.cz>
2016-04-22 11:44:33 +0200

    test btnUnderline

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -58,6 +58,11 @@ class PythonOrgSearch(unittest.TestCase):
         driver.find_element_by_id('btnItalic').click()
         self.assertTrue("~~  ~~", driver.find_element_by_id("editor").get_attribute('value'))
 
+    def test_underline_btn(self):
+        driver = self.driver
+        driver.find_element_by_id('btnUnderline').click()
+        self.assertTrue("__  __", driver.find_element_by_id("editor").get_attribute('value'))
+
     def tearDown(self):
         self.driver.close()
 
