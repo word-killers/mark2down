87813954c5611125b369cef4bcdb3d0885d32030
Jiřina Chocholková <chocholkovaj@seznam.cz>
2016-05-02 18:56:54 +0200

    added element .is_displayed()

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -42,144 +42,200 @@ class PythonOrgSearch(unittest.TestCase):
 
     def test_graph_btn(self):
         driver = self.driver
-        driver.find_element_by_id('btnGraph').click()
+        btn = driver.find_element_by_id('btnGraph')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         self.assertEqual("```graph\n\n```", driver.find_element_by_id("editor").get_attribute('value'))
 
     def test_table_btn(self):
         driver = self.driver
-        driver.find_element_by_id('tableButton').click()
+        btn = driver.find_element_by_id('tableButton')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         self.assertTrue(driver.find_element_by_id('dialog').is_displayed())
 
     def test_H1_btn(self):
         driver = self.driver
-        driver.find_element_by_id('btnH1').click()
+        btn = driver.find_element_by_id('btnH1')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         self.assertEqual("# ", driver.find_element_by_id("editor").get_attribute('value'))
 
     def test_H2_btn(self):
         driver = self.driver
-        driver.find_element_by_id('btnH2').click()
+        btn = driver.find_element_by_id('btnH2')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         self.assertEqual("## ", driver.find_element_by_id("editor").get_attribute('value'))
 
     def test_H3_btn(self):
         driver = self.driver
-        driver.find_element_by_id('btnH3').click()
+        btn = driver.find_element_by_id('btnH3')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         self.assertEqual("### ", driver.find_element_by_id("editor").get_attribute('value'))
 
     def test_H4_btn(self):
         driver = self.driver
-        driver.find_element_by_id('btnH4').click()
+        btn = driver.find_element_by_id('btnH4')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         self.assertEqual("#### ", driver.find_element_by_id("editor").get_attribute('value'))
 
     def test_H5_btn(self):
         driver = self.driver
-        driver.find_element_by_id('btnH5').click()
+        btn = driver.find_element_by_id('btnH5')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         self.assertEqual("##### ", driver.find_element_by_id("editor").get_attribute('value'))
 
     def test_H6_btn(self):
         driver = self.driver
-        driver.find_element_by_id('btnH6').click()
+        btn = driver.find_element_by_id('btnH6')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         self.assertEqual("###### ", driver.find_element_by_id("editor").get_attribute('value'))
 
     def test_bold_btn(self):
         driver = self.driver
-        driver.find_element_by_id('btnBold').click()
+        btn = driver.find_element_by_id('btnBold')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         self.assertEqual("++  ++", driver.find_element_by_id("editor").get_attribute('value'))
 
     def test_italic_btn(self):
         driver = self.driver
-        driver.find_element_by_id('btnItalic').click()
+        btn = driver.find_element_by_id('btnItalic')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         self.assertEqual("~~  ~~", driver.find_element_by_id("editor").get_attribute('value'))
 
     def test_underline_btn(self):
         driver = self.driver
-        driver.find_element_by_id('btnUnderline').click()
+        btn = driver.find_element_by_id('btnUnderline')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         self.assertEqual("__  __", driver.find_element_by_id("editor").get_attribute('value'))
 
     def test_strikeThrough_btn(self):
         driver = self.driver
-        driver.find_element_by_id('btnStrikeThrough').click()
+        btn = driver.find_element_by_id('btnStrikeThrough')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         self.assertEqual("--  --", driver.find_element_by_id("editor").get_attribute('value'))
 
     def test_typeWriting_btn(self):
         driver = self.driver
-        driver.find_element_by_id('btnTypewriting').click()
+        btn = driver.find_element_by_id('btnTypewriting')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         self.assertEqual("```  ```", driver.find_element_by_id("editor").get_attribute('value'))
 
     def test_alignLeft_btn(self):
         driver = self.driver
-        driver.find_element_by_id('btnAlignLeft').click()
+        btn = driver.find_element_by_id('btnAlignLeft')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         self.assertEqual("{{\n", driver.find_element_by_id("editor").get_attribute('value'))
 
     def test_alignCenter_btn(self):
         driver = self.driver
-        driver.find_element_by_id('btnAlignCenter').click()
+        btn = driver.find_element_by_id('btnAlignCenter')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         self.assertEqual("}{\n", driver.find_element_by_id("editor").get_attribute('value'))
 
     def test_alignBlock_btn(self):
         driver = self.driver
-        driver.find_element_by_id('btnAlignBlock').click()
+        btn = driver.find_element_by_id('btnAlignBlock')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         self.assertEqual("{}\n", driver.find_element_by_id("editor").get_attribute('value'))
 
     def test_alignRight_btn(self):
         driver = self.driver
-        driver.find_element_by_id('btnAlignRight').click()
+        btn = driver.find_element_by_id('btnAlignRight')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         self.assertEqual("}}\n", driver.find_element_by_id("editor").get_attribute('value'))
 
     def test_cislovanySeznam_btn(self):
         driver = self.driver
-        driver.find_element_by_id('btnNumerate').click()
+        btn = driver.find_element_by_id('btnNumerate')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         self.assertEqual("1. ", driver.find_element_by_id("editor").get_attribute('value'))
 
     def test_odrazkovySeznam_btn(self):
         driver = self.driver
-        driver.find_element_by_id('btnList').click()
+        btn = driver.find_element_by_id('btnList')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         self.assertEqual("- ", driver.find_element_by_id("editor").get_attribute('value'))
 
     def test_include_btn(self):
         driver = self.driver
-        driver.find_element_by_id('btnInclude').click()
+        btn = driver.find_element_by_id('btnInclude')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         self.assertEqual("{!  !}", driver.find_element_by_id("editor").get_attribute('value'))
 
     def test_image_btn(self):
         driver = self.driver
-        driver.find_element_by_id('btnImage').click()
+        btn = driver.find_element_by_id('btnImage')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         self.assertEqual("![alt text](image path \"Tooltip text\")", driver.find_element_by_id("editor").get_attribute('value'))
 
     def test_code_btn(self):
         driver = self.driver
-        driver.find_element_by_id('btnCode').click()
+        btn = driver.find_element_by_id('btnCode')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         self.assertEqual("```\n\t\n```", driver.find_element_by_id("editor").get_attribute('value'))
 
     def test_preview_btn(self):
        driver = self.driver
-       driver.find_element_by_id('previewOpen').click()
+       btn = driver.find_element_by_id('previewOpen')
+       self.assertTrue(btn.is_displayed())
+       btn.click()
        wait = WebDriverWait(driver, 5)
        wait.until(EC.visibility_of(driver.find_element_by_id('previewDialog')))
        self.assertTrue(driver.find_element_by_id('previewDialog').is_displayed())
 
     def test_renderMermaid_btn(self):
         driver = self.driver
-        driver.find_element_by_id('mermaidBtn').click()
+        btn = driver.find_element_by_id('mermaidBtn')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         # TODO
 
     def test_share_btn(self):
         driver = self.driver
-        driver.find_element_by_id('btnShare').click()
+        btn = driver.find_element_by_id('btnShare')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         # TODO
 
     def test_export_btn(self):
         driver = self.driver
-        driver.find_element_by_id('btnExport').click()
+        btn = driver.find_element_by_id('btnExport')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         # TODO
 
     def test_print_btn(self):
         driver = self.driver
-        driver.find_element_by_id('btnPrint').click()
+        btn = driver.find_element_by_id('btnPrint')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         # TODO
 
     def test_login_btn(self):
         driver = self.driver
-        driver.find_element_by_id('btnLogin').click()
+        btn = driver.find_element_by_id('btnLogin')
+        self.assertTrue(btn.is_displayed())
+        btn.click()
         # TODO
 
     def tearDown(self):
